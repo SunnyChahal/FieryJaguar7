b4cacd3d12778ce8c20644d208fa8f78e562c8b3
Corwin Shanner <corwin.shanner@gmail.com>
2016-09-15 16:22:32 -0700

    finished datapath, reg4

--- lab1/datapath.v
+++ lab1/datapath.v
@@ -11,7 +11,31 @@ input load_dcard1, load_dcard2, load_dcard3;
 output [3:0] pcard3_out;
 output [3:0] pscore_out, dscore_out;
 output [6:0] HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
-						
+
+wire [3:0] new_card, pcard1_out, pcard2_out, pcard3_out, dcard1_out, dcard2_out, dcard3_out;
+
+//dealer
+dealcard DEALER (.clock(fast_clock), .resetb(resetb), .new_card(new_card));
+
+//card value registers	
+reg4 PCARD1 (.in(new_card), .load(load_pcard1), .rst(resetb), .clk(slow_clock), .out(pcard1_out));
+reg4 PCARD2 (.in(new_card), .load(load_pcard2), .rst(resetb), .clk(slow_clock), .out(pcard2_out));
+reg4 PCARD3 (.in(new_card), .load(load_pcard3), .rst(resetb), .clk(slow_clock), .out(pcard3_out));
+reg4 DCARD1 (.in(new_card), .load(load_dcard1), .rst(resetb), .clk(slow_clock), .out(dcard1_out));
+reg4 DCARD2 (.in(new_card), .load(load_dcard2), .rst(resetb), .clk(slow_clock), .out(dcard2_out));
+reg4 DCARD3 (.in(new_card), .load(load_dcard3), .rst(resetb), .clk(slow_clock), .out(dcard3_out));
+
+//card 7seg drivers
+card7seg PC1_7SEG (.SW(pcard1_out), .HEX0(HEX0));
+card7seg PC2_7SEG (.SW(pcard2_out), .HEX0(HEX1));
+card7seg PC3_7SEG (.SW(pcard3_out), .HEX0(HEX2));
+card7seg DC1_7SEG (.SW(dcard1_out), .HEX0(HEX3));
+card7seg DC2_7SEG (.SW(dcard2_out), .HEX0(HEX4));
+card7seg DC3_7SEG (.SW(dcard3_out), .HEX0(HEX5));
+
+//hand scorers
+scorehand PSCORER (.card1(pcard1_out), .card2(pcard2_out), .card3(pcard3_out), .total(pscore_out));
+scorehand DSCORER (.card1(dcard1_out), .card2(dcard2_out), .card3(dcard3_out), .total(dscore_out));
 
 // The code describing your datapath will go here.  Your datapath 
 // will hierarchically instantiate six card7seg blocks, two scorehand
---
+++ lab1/reg4.v
@@ -0,0 +1,8 @@
+module reg4 (in, load, rst, clk, out) begin
+	input [3:0] in;
+	input load, rst, clk;
+	output [3:0] out;
+	
+	//always block for a 4-bit wide register with asynchronous reset
+	
+endmodule
