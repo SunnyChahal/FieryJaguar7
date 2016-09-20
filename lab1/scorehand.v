d5f2af4a1deeb4748c9ad6c48f9f2d6fa5f5cf1d
Corwin Shanner <corwin.shanner@gmail.com>
2016-09-19 21:20:21 -0700

    fixed scorehand

--- lab1/scorehand.v
+++ lab1/scorehand.v
@@ -3,12 +3,16 @@ module scorehand (card1, card2, card3, total);
 
 input [3:0] card1, card2, card3;
 output [3:0] total;
+wire [3:0] card1value, card2value, card3value;
 
 // The code describing scorehand will go here.  Remember this is a combinational
 // block.  The function is described in the handout.  Be sure to read the section
 // on representing numbers in Slide Set 2.
 
-assign total = (card1 + card2 + card3) % 10;
+assign card1value = (card1 > 0'b1001) ? 0'b0000 : card1;
+assign card3value = (card2 > 0'b1001) ? 0'b0000 : card2;
+assign card3value = (card3 > 0'b1001) ? 0'b0000 : card3;
+
+assign total = (card1value + card2value + card3value) % 10;
 
 endmodule
-	
