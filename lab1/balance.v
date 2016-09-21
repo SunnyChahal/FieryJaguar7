854b26038e6acb61bbf1ed8afb6efbd0e9b4a87c
Sunmeet Chahal <Sunmeet Chahal>
2016-09-20 22:08:09 -0700

    this is the new balance file

--- lab1/balance.v
+++ lab1/balance.v
@@ -1,40 +1,42 @@
-module balance (endround, SW, pscore, dscore, LEDR)
-	input [7:0] SW, balance;
+module balance (endround, reset, SW, pscore, dscore, LEDR);
+	input [9:0] SW;
 	input endround;
+	input reset;
 	input [3:0] pscore, dscore;
-	output [0:7] LEDR;
-	reg [7:0] balance;
+	output [7:0] LEDR;
 	
+	reg [7:0] balance;    // we are initially letting the gambler start with 100$. She can choose to keep betting until she becomes broke i.e. 0$ balance or earn upto a maximum of 512 $ balance so we use 8 bits.
+   balance = reset ? balance : 8'b00110001;
 	assign LEDR[7:0] = balance;
 	
 	//bits in winner:          pwin     |       dwin     |       tie
 	wire [2:0] winner = {pscore > dscore, pscore < dscore, pscore == dscore};
 	//bits in bet:         pbet     |      dbet     |      tbet
-	wire [2:0] bet = {SW[9] & ~SW[8], SW[8] & ~SW[9], SW[8] & SW[9]}
+	wire [2:0] bet = {SW[9] & ~SW[8], SW[8] & ~SW[9], SW[8] & SW[9]};
 	
 	//synchronous module that computes balance every time the round is over (i.e. in `END state)
 	always @(posedge endround) begin
 		case (winner)
 			3'b001: //tie
-				if (tbet) begin				//bet on tie,    +%800 payout
+				if (bet == 3'b001) begin				//bet on tie,    +%800 payout
 					LEDR[7:0] <= balance + (SW[7:0] * 8);
-				end else if (pbet) begin	//bet on player, -%100 payout
+				end else if (bet == 3'b100) begin	//bet on player, -%100 payout
 					LEDR[7:0] <= balance - SW[7:0];
 				end else begin				//bet on dealer, -%100 payout
 					LEDR[7:0] <= balance - SW[7:0];
 				end
-			2'b010: //player won
-				if (tbet) begin				//bet on tie,    -%100 payout
+			3'b010: //player won
+				if (bet == 3'b001) begin				//bet on tie,    -%100 payout
 					LEDR[7:0] <= balance - SW[7:0];
-				end else if (pbet) begin	//bet on player, +%100 payout
+				end else if (bet == 3'b100) begin	//bet on player, +%100 payout
 					LEDR[7:0] <= balance + SW[7:0];
 				end else begin				//bet on dealer, -%100 payout
 					LEDR[7:0] <= balance - SW[7:0];
 				end
-			2'b001:	//dealer won
-				if (tbet) begin				//bet on tie,    -%100 payout
+			3'b001:	//dealer won
+				if (bet == 3'b001) begin				//bet on tie,    -%100 payout
 					LEDR[7:0] <= balance - SW[7:0];
-				end else if (pbet) begin	//bet on player, -%100 payout
+				end else if (bet == 3'b100) begin	//bet on player, -%100 payout
 					LEDR[7:0] <= balance - SW[7:0];
 				end else begin				//bet on dealer, +%95 payout
 					LEDR[7:0] <= balance + SW[7:0];
